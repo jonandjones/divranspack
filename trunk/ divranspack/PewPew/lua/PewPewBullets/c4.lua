563b11d821f511bc77b5bb7600f56ddd2c495c7e
arvid_divran@hotmail.com <arvid_divran@hotmail.com@8cfe23f0-34c4-11de-8a2e-13aa1706fec1>
2010-02-26 23:08:34 +0000

    -- Changed folder name of c4 explosion -- Added WTF BOOM bomb

--- trunk/ divranspack/PewPew/lua/PewPewBullets/c4.lua
+++ trunk/ divranspack/PewPew/lua/PewPewBullets/c4.lua
@@ -19,7 +19,7 @@ BULLET.Trail = nil
 -- Effects / Sounds
 BULLET.FireSound = {"weapons/explode1.wav","weapons/explode2.wav"}
 BULLET.ExplosionSound = nil
-BULLET.FireEffect = "c4_explosion" --"dirtyxplo"
+BULLET.FireEffect = "pewpew_c4_explosion"
 BULLET.ExplosionEffect = nil
 
 -- Movement
