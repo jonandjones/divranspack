e4dfc7e159ddacac15f15d1f0d6df24f29d4405e
arvid_divran <arvid_divran@hotmail.com>
2010-03-10 20:15:50 +0000

    - Fixed cores not working

---  divranspack/PewPew/lua/entities/pewpew_core/init.lua
+++  divranspack/PewPew/lua/entities/pewpew_core/init.lua
@@ -16,8 +16,8 @@ function ENT:Initialize()
 	self.pewpew.CoreMaxHealth = 1
 	self.Entity.Core = self
 	
-	Wire_TriggerOutput( self.Entity, "Health", self.pewpewCoreHealth or 0 )
-	Wire_TriggerOutput( self.Entity, "Total Health", self.pewpewCoreMaxHealth or 0 )
+	Wire_TriggerOutput( self.Entity, "Health", self.pewpew.CoreHealth or 0 )
+	Wire_TriggerOutput( self.Entity, "Total Health", self.pewpew.CoreMaxHealth or 0 )
 	
 	self.Entity:NextThink( CurTime() + 1 )
 	return true
@@ -41,21 +41,23 @@ function ENT:Think()
 	self.Props = constraint.GetAllConstrainedEntities( self.Entity )
 	
 	-- Loop through all props
-	local hp = self.pewpewCoreHealth
+	local hp = self.pewpew.CoreHealth
 	local maxhp = 1
 	for _, ent in pairs( self.Props ) do
+		if (!ent.pewpew) then ent.pewpew = {} end
 		if (ent and pewpew:CheckValid( ent ) and pewpew:CheckAllowed( ent )) then
+			local entcore = ent.pewpew.Core
 			local health = self.PropHealth[ent:EntIndex()] or 0
 			local enthealth = pewpew:GetHealth( ent )
-			if (!ent.Core or !pewpew:CheckValid(ent.Core)) then -- if the entity has no core
-				ent.Core = self.Entity
+			if (!entcore or !pewpew:CheckValid(entcore)) then -- if the entity has no core
+				ent.pewpew.Core = self.Entity
 				self.PropHealth[ent:EntIndex()] = enthealth
 				hp = hp + enthealth
-			elseif (ent.Core and pewpew:CheckValid(ent.Core) and ent.Core == self.Entity and enthealth != health) then -- if the entity's health has changed
+			elseif (entcore and pewpew:CheckValid(entcore) and entcore == self.Entity and enthealth != health) then -- if the entity's health has changed
 				hp = hp - health -- subtract the old health
 				hp = hp + enthealth -- add the new health
 				self.PropHealth[ent:EntIndex()] = enthealth
-			elseif (ent.Core and pewpew:CheckValid( ent.Core ) and ent.Core != self.Entity) then -- if the entity already has a core
+			elseif (entcore and pewpew:CheckValid( entcore ) and entcore != self.Entity) then -- if the entity already has a core
 				self.Owner:ChatPrint("You cannot have several cores in the same contraption. Core self-destructing.")
 					local effectdata = EffectData()
 					effectdata:SetOrigin( self.Entity:GetPos() )
@@ -68,26 +70,26 @@ function ENT:Think()
 		end
 	end
 	-- Set health
-	self.pewpewCoreHealth = hp
-	self.pewpewCoreMaxHealth = maxhp
+	self.pewpew.CoreHealth = hp
+	self.pewpew.CoreMaxHealth = maxhp
 	
 	
 	-- Set NW ints
 	hp = self.Entity:GetNWInt("pewpewMaxHealth")
-	if (!hp or hp != self.pewpewCoreMaxHealth) then
-		self.Entity:SetNWInt("pewpewMaxHealth", self.pewpewCoreMaxHealth)
+	if (!hp or hp != self.pewpew.CoreMaxHealth) then
+		self.Entity:SetNWInt("pewpewMaxHealth", self.pewpew.CoreMaxHealth)
 	end
 	hp = self.Entity:GetNWInt("pewpewHealth")
-	if (!hp or hp != self.pewpewCoreHealth) then
-		self.Entity:SetNWInt("pewpewHealth", self.pewpewCoreHealth)
+	if (!hp or hp != self.pewpew.CoreHealth) then
+		self.Entity:SetNWInt("pewpewHealth", self.pewpew.CoreHealth)
 	end
 	
 	-- Wire Output
-	Wire_TriggerOutput( self.Entity, "Health", self.pewpewCoreHealth or 0 )
-	Wire_TriggerOutput( self.Entity, "Total Health", self.pewpewCoreMaxHealth or 0 )
+	Wire_TriggerOutput( self.Entity, "Health", self.pewpew.CoreHealth or 0 )
+	Wire_TriggerOutput( self.Entity, "Total Health", self.pewpew.CoreMaxHealth or 0 )
 	
 	-- Run again in 1 second
-	self.Entity:NextThink( CurTime() + 1 )
+	self.Entity:NextThink( CurTime() + 5 )
 	return true
 end
 
