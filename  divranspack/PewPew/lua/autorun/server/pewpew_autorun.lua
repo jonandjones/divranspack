e580d05c11bb6d9cb417fa25d4dc6be81b4fa853
arvid_divran <arvid_divran@hotmail.com>
2010-03-19 16:28:04 +0000

    ---- PewPew: - Added PewPew to tags

---  divranspack/PewPew/lua/autorun/server/pewpew_autorun.lua
+++  divranspack/PewPew/lua/autorun/server/pewpew_autorun.lua
@@ -14,3 +14,9 @@ AddCSLuaFile("pewpew_gcombatcompability.lua")
 include("pewpew_gcombatcompability.lua")
 
 pewpew:LoadBullets()
+
+-- Tags
+local tags = GetConVar( "sv_tags" )
+if (!string.find( tags, "PewPew" )) then
+	RunConsoleCommand( "sv_tags", GetConVar( "sv_tags" ):GetString() .. ",PewPew" )
+end
