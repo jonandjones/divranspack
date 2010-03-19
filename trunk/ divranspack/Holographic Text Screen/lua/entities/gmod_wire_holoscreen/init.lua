821f15b949c9f10d9abf0349fe42e5e3dea4fcee
arvid_divran@hotmail.com <arvid_divran@hotmail.com@8cfe23f0-34c4-11de-8a2e-13aa1706fec1>
2010-03-19 23:52:23 +0000

    ---- Holoscreen: - Added angle input

--- trunk/ divranspack/Holographic Text Screen/lua/entities/gmod_wire_holoscreen/init.lua
+++ trunk/ divranspack/Holographic Text Screen/lua/entities/gmod_wire_holoscreen/init.lua
@@ -59,36 +59,50 @@ function ENT:Initialize()
 	self.Text = "Holoscreen - By Divran"
 	self.TextColor = Color(255,255,255,255)
 	self.PosOffset = Vector(0,0,0)
+	self.AngOffset = Angle(0,0,0)
 	self.Size = 1
 	self.Font = "Default"
 	
-	self.Inputs = WireLib.CreateInputs( self.Entity, { "Text [STRING]", "Color [VECTOR]", "Alpha", "Size", "Offset Pos [VECTOR]", "Font [STRING]" } )
+	self.Inputs = WireLib.CreateInputs( self.Entity, { "Text [STRING]", "Color [VECTOR]", "Alpha", "Size", "Offset Pos [VECTOR]", "Offset Angle [ANGLE]", "Font [STRING]" } )
 	
 	self:SetOverlayText( "Holographic Screen" )
-	self:UpdateText()
+	self:UpdateAll()
 end
 
 function ENT:TriggerInput( name, value )
 	if (name == "Text") then
 		self.Text = value
+		self:SetNWString("holoscreen_text", self.Text )
 	elseif (name == "Color") then
 		self.TextColor = Color(math.Clamp(value.x,0,255),math.Clamp(value.y,0,255),math.Clamp(value.z,0,255), self.TextColor.a)
+		self:SetNWInt("holoscreen_color_r", self.TextColor.r )
+		self:SetNWInt("holoscreen_color_g", self.TextColor.g )
+		self:SetNWInt("holoscreen_color_b", self.TextColor.b )
 	elseif (name == "Alpha") then
 		self.TextColor.a = math.Clamp(value,0,255)
+		self:SetNWInt("holoscreen_color_a", self.TextColor.a )
 	elseif (name == "Size") then
 		self.Size = math.Clamp(value,0.1,5)
+		self:SetNWInt("holoscreen_size", self.Size )
 	elseif (name == "Offset Pos") then
 		self.PosOffset = Vector(math.Clamp(value.x,-200,200),math.Clamp(value.y,-200,200),math.Clamp(value.z,-200,200))
+		self:SetNWInt("holoscreen_pos_x", self.PosOffset.x )
+		self:SetNWInt("holoscreen_pos_y", self.PosOffset.y )
+		self:SetNWInt("holoscreen_pos_z", self.PosOffset.z )
 	elseif (name == "Font") then
 		if (table.HasValue( self.AllowedFonts, value )) then
 			self.Font = value
+			self:SetNWString("holoscreen_font", self.Font )
 		end
+	elseif (name == "Offset Angle") then
+		self.AngOffset = value
+		self:SetNWInt("holoscreen_ang_p", self.AngOffset.p )
+		self:SetNWInt("holoscreen_ang_y", self.AngOffset.y )
+		self:SetNWInt("holoscreen_ang_r", self.AngOffset.r )
 	end
-		
-	self:UpdateText()
 end
 
-function ENT:UpdateText()
+function ENT:UpdateAll()
 	self:SetNWString("holoscreen_text", self.Text )
 	self:SetNWInt("holoscreen_color_r", self.TextColor.r )
 	self:SetNWInt("holoscreen_color_g", self.TextColor.g )
@@ -97,6 +111,9 @@ function ENT:UpdateText()
 	self:SetNWInt("holoscreen_pos_x", self.PosOffset.x )
 	self:SetNWInt("holoscreen_pos_y", self.PosOffset.y )
 	self:SetNWInt("holoscreen_pos_z", self.PosOffset.z )
+	self:SetNWInt("holoscreen_ang_p", self.AngOffset.p )
+	self:SetNWInt("holoscreen_ang_y", self.AngOffset.y )
+	self:SetNWInt("holoscreen_ang_r", self.AngOffset.r )
 	self:SetNWString("holoscreen_font", self.Font )
 	self:SetNWInt("holoscreen_size", self.Size )
 end
