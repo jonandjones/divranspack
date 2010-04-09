2ba8e0d0328332ad11f981c7adb0b8d37c4e74bf
arvid_divran@hotmail.com <arvid_divran@hotmail.com@8cfe23f0-34c4-11de-8a2e-13aa1706fec1>
2010-04-09 18:06:07 +0000

    ---- PewPew - Fixed death notices. They no longer show only "pewpew_base_bullet" or "pewpew_base_cannon". They now show the name of the weapon instead.

---
+++ trunk/ divranspack/PewPew/lua/pewpew_deathnotice.lua
@@ -0,0 +1,48 @@
+-- PewPew DeathNotice
+-- Fixes the death notice so that it doesn't just say "pewpew_base_bullet"
+
+-- Wait 1 second to give the gamemode time to load
+timer.Simple(1,function()
+	-- Save the PlayerDeath function
+	local OldFunc = GAMEMODE.PlayerDeath
+	-- Override the PlayerDeath function
+	function GAMEMODE:PlayerDeath( Victim, Inflictor, Attacker )
+		-- Check if the attacker is a pewpew bullet
+		if (Attacker:GetClass() == "pewpew_base_bullet") then
+			-- Check if its name is valid
+			if (Attacker.Bullet and Attacker.Bullet.Name and Attacker.Bullet.Name != "") then
+				-- Send umsg
+				umsg.Start("PlayerKilled")
+					umsg.Entity( Victim )
+					umsg.String( "suicide" )
+					umsg.String( Attacker.Bullet.Name )
+				umsg.End()
+			end
+			
+			-- Set spawn time
+			Victim.NextSpawnTime = CurTime() + 2
+			Victim.DeathTime = CurTime()
+			return
+		end
+		
+		-- Check if the attacker is a pewpew cannon
+		if (Attacker:GetClass() == "pewpew_base_cannon") then
+			-- Check if its name is valid
+			if (Attacker.Bullet and Attacker.Bullet.Name and Attacker.Bullet.Name != "") then
+				-- Send umsg
+				umsg.Start("PlayerKilled")
+					umsg.Entity( Victim )
+					umsg.String( "suicide" )
+					umsg.String( Attacker.Bullet.Name )
+				umsg.End()
+			end
+			
+			-- Set spawn time
+			Victim.NextSpawnTime = CurTime() + 2
+			Victim.DeathTime = CurTime()
+			return
+		end
+		
+		OldFunc( GAMEMODE, Victim, Inflictor, Attacker )
+	end
+end)
