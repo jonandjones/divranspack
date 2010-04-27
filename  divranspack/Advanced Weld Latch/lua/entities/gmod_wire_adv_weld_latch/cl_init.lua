074b7224ce31390887056d0111eee911a2d7d84c
arvid_divran <arvid_divran@hotmail.com>
2010-04-27 07:25:20 +0000

    ---- Adv Weld Latch ---- NoCollide Remover

---
+++  divranspack/Advanced Weld Latch/lua/entities/gmod_wire_adv_weld_latch/cl_init.lua
@@ -0,0 +1,7 @@
+-- Wire Advanced Weld Latch
+-- Made by Divran
+
+include('shared.lua')
+
+ENT.RenderGroup 		= RENDERGROUP_OPAQUE
+
---
+++  divranspack/Advanced Weld Latch/lua/entities/gmod_wire_adv_weld_latch/shared.lua
@@ -0,0 +1,9 @@
+-- Wire Advanced Weld Latch
+-- Made by Divran
+
+ENT.Type        = "anim"
+ENT.Base        = "base_wire_entity"
+
+ENT.PrintName   = "Adv Wire Weld Latch"
+ENT.Author      = "Divran"
+ENT.Contact     = ""
