24fe750e34875150b625b9a8d17c51778c89b3ab
arvid_divran <arvid_divran@hotmail.com>
2010-08-16 15:55:28 +0000

    http://www.wiremod.com/forum/developers-showcase/20727-egp-v3-total-remake-32.html#post208188

---  divranspack/EGP v3 beta/lua/entities/gmod_wire_egp_hud/cl_init.lua
+++  divranspack/EGP v3 beta/lua/entities/gmod_wire_egp_hud/cl_init.lua
@@ -1,40 +1,67 @@
 include('shared.lua')
 include("HUDDraw.lua")
 
---[[
-function ENT:ChangePositions( Obj )
+local function to512( n, n2 )
+	return n / 512 * n2
+end
+
+local function toScreenRes( n, n2 )
+	return n / n2 * 512
+end
+
+function ENT:ChangePositions( Obj, bool )
 	local w, h = ScrW(), ScrH()
-	if (Obj.x) then Obj.x = Obj.x/512*w end
-	if (Obj.y) then Obj.y = Obj.y/512*h end
-	if (Obj.x2) then Obj.x2 = Obj.x2/512*w end
-	if (Obj.y2) then Obj.y2 = Obj.y2/512*h end
-	if (Obj.x3) then Obj.x3 = Obj.x3/512*w end
-	if (Obj.y3) then Obj.y3 = Obj.y3/512*h end
+	
+	local func
+	if (bool) then
+		func = to512
+	else
+		func = toScreenRes
+	end
+	
+	if (Obj.x) then Obj.x = func(Obj.x,w) end
+	if (Obj.y) then Obj.y = func(Obj.y,h) end
+	if (Obj.x2) then Obj.x2 = func(Obj.x2,w) end
+	if (Obj.y2) then Obj.y2 = func(Obj.y2,h) end
+	if (Obj.x3) then Obj.x3 = func(Obj.x3,w) end
+	if (Obj.y3) then Obj.y3 = func(Obj.y3,h) end
 	if (Obj.vertices) then
 		for k,v in ipairs( Obj.vertices ) do
-			v.x = v.x/512*w
-			v.y = v.y/512*h
+			v.x = func(v.x,w)
+			v.y = func(v.y,h)
 		end
 	end
-	if (Obj.size and Obj.ID != EGP.Objects.Names["Text"]) then Obj.size = Obj.size/512*math.sqrt(w^2+h^2) end
-	if (Obj.w) then Obj.w = Obj.w/512*w end
-	if (Obj.h) then Obj.h = Obj.h/512*h end
+	if (Obj.size and Obj.ID != EGP.Objects.Names["Text"] and Obj.ID != EGP.Objects.Names["TextLayout"]) then 
+		if (bool) then
+			Obj.size = Obj.size/512*math.sqrt(w^2+h^2)
+		else
+			Obj.size = Obj.size/math.sqrt(w^2+h^2)*512
+		end
+	end
+			
+	if (Obj.w) then Obj.w = func(Obj.w,w) end
+	if (Obj.h) then Obj.h = func(Obj.h,h) end
 end
-]]
 
 function ENT:Initialize()
 	self.RenderTable = {}
+	self.Resolution = false -- False = Use screen res. True = 0-512 res.
+	self.OldResolution = false
 	
 	EGP:AddHUDEGP( self )
 end
 
 function ENT:EGP_Update() 
 	for k,v in ipairs( self.RenderTable ) do
+		if (self.Resolution != self.OldResolution) then 
+			self:ChangePositions( v, self.Resolution )
+		end
 		if (v.parent and v.parent != 0) then
 			local x, y, angle = EGP:GetGlobalPos( self, v.index )
 			EGP:EditObject( v, { x = x, y = y, angle = angle } )
 		end
 	end
+	self.OldResolution = self.Resolution
 end
 
 function ENT:OnRemove()
@@ -42,6 +69,10 @@ function ENT:OnRemove()
 end
 
 function ENT:Draw()
+	self.Resolution = self:GetNWBool("Resolution",false)
+	if (self.Resolution != self.OldResolution) then
+		self:EGP_Update()
+	end
 	self.Entity.DrawEntityOutline = function() end
 	self.Entity:DrawModel()
 	Wire_Render(self.Entity)
